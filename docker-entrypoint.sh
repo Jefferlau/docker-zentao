deb37ffae6980c4c8f81d99d9931fd20413afd9f
Tiyang Liu <jefferlzu@gmail.com>
2019-08-12 10:29:54 +0800

    without database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,6 @@ if [ "`cat /app/zentaopms/VERSION`" != "`cat /var/www/zentaopms/VERSION`" ]; the
   cp -a /var/www/zentaopms/* /app/zentaopms
 fi
 
-chmod -R 777 /app/zentaopms/www/data
 chmod -R 777 /app/zentaopms/tmp
 chmod 777 /app/zentaopms/www
 chmod 777 /app/zentaopms/config
@@ -19,14 +18,5 @@ chmod -R a+rx /app/zentaopms/bin/*
 /etc/init.d/apache2 start
 
 chown -R www-data:www-data /app/zentaopms
-chown -R mysql:mysql /var/lib/mysql/
-if [ "`ls -A /var/lib/mysql/`" = "" ]; then
-  mysql_install_db --defaults-file=/etc/mysql/my.cnf
-  /etc/init.d/mysql start
-  /usr/bin/mysqladmin -uroot password $MYSQL_ROOT_PASSWORD
-  mysql -uroot -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';FLUSH PRIVILEGES;"
-else
-  /etc/init.d/mysql start
-fi
 
 tail -f /var/log/apache2/zentao_error_log
